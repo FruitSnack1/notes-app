fd7441981cbc7d4b2ca305d7347945466e0568a4
FruitSnack1 <tomas.ryzner@email.cz>
2022-10-29 10:22:42 +0200

    prod api url

--- api/http-common.js
+++ api/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: 'http://localhost:3001',
+  // baseURL: 'http://localhost:3001',
+  baseURL: 'https://good-habits.fly.dev',
   withCredentials: true,
   headers: {
     'Content-type': 'application/json',
