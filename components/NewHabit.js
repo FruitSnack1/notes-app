4d8eafea13d7e9dfda29d36015a021f93bdeaa26
FruitSnack1 <tomas.ryzner@email.cz>
2022-11-04 11:09:59 +0100

    new habit ui

--- components/NewHabit.js
+++ components/NewHabit.js
@@ -1,9 +1,11 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import React, { useState } from 'react'
 import { postHabit } from '../api/habit.api'
+import { Card } from './Card'
 
 export const NewHabit = () => {
   const [title, setTitle] = useState('')
+  const [visible, setVisible] = useState(false)
 
   const queryClient = useQueryClient()
 
@@ -11,36 +13,67 @@ export const NewHabit = () => {
     onSuccess: () => {
       queryClient.invalidateQueries(['habits'])
       setTitle('')
+      setVisible(false)
     },
   })
 
   return (
     <>
-      <form>
-        <h3>Create new Habit</h3>
-        <div className='mb-3'>
-          <label className='form-label'>Habit title</label>
-          <input
-            type='text'
-            className='form-control'
-            id='exampleInputEmail1'
-            aria-describedby='emailHelp'
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-          />
-        </div>
-
-        <button
-          type='submit'
-          className='btn btn-primary text-white'
+      <div
+        className={`position-absolute h-100 w-100 top-0 start-0 backdrop ${
+          visible ? 'visible' : 'invisible'
+        }`}
+      >
+        <Card className='ms-auto me-auto w-50 mt-5'>
+          <form>
+            <h3 className='fw-bold'>Create new Habit</h3>
+            <div className='mb-3'>
+              <label className='form-label'>Habit title</label>
+              <input
+                type='text'
+                className='form-control'
+                id='exampleInputEmail1'
+                placeholder='Drink water...'
+                aria-describedby='emailHelp'
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+              />
+            </div>
+            <div className='d-flex justify-content-end'>
+              <button
+                className='btn btn-secondary fw-bold me-3'
+                onClick={(e) => {
+                  e.preventDefault()
+                  setVisible(false)
+                  setTitle('')
+                }}
+              >
+                Cancel
+              </button>
+              <button
+                type='submit'
+                className='btn btn-primary text-white '
+                onClick={(e) => {
+                  e.preventDefault()
+                  mutation.mutate()
+                }}
+              >
+                Create habit
+              </button>
+            </div>
+          </form>
+        </Card>
+      </div>
+      <div class='d-grid gap-2'>
+        <btn
           onClick={(e) => {
-            e.preventDefault()
-            mutation.mutate()
+            setVisible(true)
           }}
+          className='btn btn-primary btn-block text-white fw-bold p-3 rounded-4 shadow'
         >
-          Create habit
-        </button>
-      </form>
+          Add new habit
+        </btn>
+      </div>
     </>
   )
 }
