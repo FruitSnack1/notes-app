f6436462fdf521cb7aa65944e3c0c8e2147d7818
FruitSnack1 <tomas.ryzner@email.cz>
2022-11-21 09:22:14 +0100

    settings

--- api/user.api.js
+++ api/user.api.js
@@ -3,3 +3,5 @@ import { apiClient } from './http-common'
 export const login = (user) => apiClient.post('/login', user)
 
 export const register = (user) => apiClient.post('/register', user)
+
+export const updateUser = (username) => apiClient.put('/user', username)
--- pages/app/settings.js
+++ pages/app/settings.js
@@ -1,10 +1,14 @@
 import { useSession } from 'next-auth/react'
 import { useState } from 'react'
+import { updateUser } from '../../api/user.api'
 import { AppLayout } from '../../layouts/AppLayout'
 import RootLayout from '../../layouts/RootLayout'
 
 export default function Settings() {
   const session = useSession()
+  console.log(session)
+
+  const [username, setUsername] = useState('')
 
   return (
     <div>
@@ -17,18 +21,17 @@ export default function Settings() {
             className='form-control'
             id='exampleInputEmail1'
             aria-describedby='emailHelp'
-            value={
-              session.status === 'authenticated'
-                ? session.data.user.username
-                : ''
-            }
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
           />
         </div>
         <button
           type='submit'
           className='btn btn-primary text-white '
-          onClick={(e) => {
+          onClick={async (e) => {
             e.preventDefault()
+            const { data } = await updateUser({ username })
+            session.data.user.name = data.username
           }}
         >
           Update
